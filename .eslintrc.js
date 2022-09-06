a8769b7ac9fdcb0a8a8b24b6bfb345ab62b1b4ed
davytautas <daniel.martins@taqtile.com.br>
2022-09-06 07:22:42 -0300

    Newlines on new files and added eol-last eslint rule to avoid the same error in future commits

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,5 +2,9 @@ module.exports = {
     extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'plugin:prettier/recommended'],
     parser: '@typescript-eslint/parser',
     plugins: ['@typescript-eslint', 'prettier'],
+    rules: {
+      'eol-last': 2
+    },
     root: true,
   };
+  
