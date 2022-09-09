ee28f6d073516d8d6767565961c3748a9d7a1e25
davytautas <daniel.martins@taqtile.com.br>
2022-09-09 19:02:47 -0300

    Created validateInput function

--- schema/resolvers.ts
+++ schema/resolvers.ts
@@ -1,8 +1,20 @@
-import { CreateUserInput, UserOutput } from './interfaces';
+import { CreateUserInput, UserInput, UserOutput } from './interfaces';
 import { User } from '../src/entity/User';
 import { AppDataSource } from '../src/data-source';
 
 const userRepo = AppDataSource.getRepository(User);
+
+async function validateInput(userData: UserInput) {
+  // A valid password should have:
+  // At least 6 characters
+  // At least 1 letter and 1 digit
+  if (userData.password.length < 6) {
+    throw new Error('Password must be at least 6 characters long.');
+  }
+  if (userData.password.length < 6) {
+    throw new Error('Password must be at least 6 characters long.');
+  }
+}
 
 export const resolvers = {
   Query: {
@@ -14,9 +26,11 @@ export const resolvers = {
 
       console.log('Inserting a new user into the database...');
       user.name = args.userData.name;
+      // The system should not allow two or more users with the same e-mail
       user.email = args.userData.email;
       user.password = args.userData.password;
       user.birthDate = args.userData.birthDate;
+      await validateInput(args.userData);
       await userRepo.save(user);
       console.log(user.name, user.id, user.email, user.password, user.birthDate);
       console.log('Saved a new user with id: ' + user.id);
@@ -25,12 +39,7 @@ export const resolvers = {
       //const users = await userRepo.find();
       //console.log('Loaded users: ', users);
 
-      return {
-        id: user.id,
-        name: user.name,
-        email: user.email,
-        birthDate: user.birthDate,
-      };
+      return user;
     },
   },
 };
