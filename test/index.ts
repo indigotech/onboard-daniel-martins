3aee1002e54670a61301099eec42acc126b35366
davytautas <daniel.martins@taqtile.com.br>
2022-09-12 12:05:26 -0300

    Initial test created and mocha added

---
+++ test/index.ts
@@ -0,0 +1,9 @@
+import 'mocha';
+
+describe('Array', function () {
+  describe('#indexOf()', function () {
+    it('should return -1 when the value is not present', function () {
+      console.log('Hello there! Testing...');
+    });
+  });
+});
