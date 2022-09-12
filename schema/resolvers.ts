a470cc0b45c25bf1be56dedf8e5d6c146fbfb945
davytautas <daniel.martins@taqtile.com.br>
2022-09-12 10:05:02 -0300

    Finished password and email input validation

--- schema/resolvers.ts
+++ schema/resolvers.ts
@@ -5,14 +5,16 @@ import { AppDataSource } from '../src/data-source';
 const userRepo = AppDataSource.getRepository(User);
 
 async function validateInput(userData: UserInput) {
-  // A valid password should have:
-  // At least 6 characters
-  // At least 1 letter and 1 digit
-  if (userData.password.length < 6) {
-    throw new Error('Password must be at least 6 characters long.');
+  const validatePW = new RegExp('^(?=.*[A-Za-z])(?=.*\\d).{6,}$');
+  if (validatePW.test(userData.password) == false) {
+    throw new Error(
+      'Password must be at least 6 characters long. Password must have at least one letter and one digit.',
+    );
   }
-  if (userData.password.length < 6) {
-    throw new Error('Password must be at least 6 characters long.');
+
+  const emailCount = await userRepo.findAndCountBy({ email: userData.email });
+  if (emailCount[1] >= 1) {
+    throw new Error('Email address already in use.');
   }
 }
 
@@ -23,22 +25,13 @@ export const resolvers = {
   Mutation: {
     async createUser(_: unknown, args: CreateUserInput): Promise<UserOutput> {
       const user = new User();
-
-      console.log('Inserting a new user into the database...');
       user.name = args.userData.name;
-      // The system should not allow two or more users with the same e-mail
       user.email = args.userData.email;
       user.password = args.userData.password;
       user.birthDate = args.userData.birthDate;
+
       await validateInput(args.userData);
       await userRepo.save(user);
-      console.log(user.name, user.id, user.email, user.password, user.birthDate);
-      console.log('Saved a new user with id: ' + user.id);
-
-      //console.log('Loading users from the database...');
-      //const users = await userRepo.find();
-      //console.log('Loaded users: ', users);
-
       return user;
     },
   },
