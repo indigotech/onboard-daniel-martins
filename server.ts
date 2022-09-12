a2da9e903a2d49f7d21f98ff2e801ad8ab1d8f7f
davytautas <daniel.martins@taqtile.com.br>
2022-09-12 14:00:20 -0300

    Further developed the initial test

--- server.ts
+++ server.ts
@@ -3,16 +3,20 @@ import 'reflect-metadata';
 import { typeDefs, resolvers } from './schema';
 import { AppDataSource } from './src/data-source';
 
-AppDataSource.initialize()
-  .then(async () => console.log('Database connection estabilished'))
-  .catch((error) => console.log(error));
+export function startServer() {
+  AppDataSource.initialize()
+    .then(async () => console.log('Database connection estabilished'))
+    .catch((error) => console.log(error));
 
-const server = new ApolloServer({
-  typeDefs,
-  resolvers,
-});
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+  });
 
-const port = 3000;
-server.listen({ port }).then(({ url }) => {
-  console.log(`Your server is present at ${url}`);
-});
+  const port = 3000;
+  server.listen({ port }).then(({ url }) => {
+    console.log(`Your server is present at ${url}`);
+  });
+}
+
+startServer();
--- test/index.ts
+++ test/index.ts
@@ -1,6 +1,10 @@
 import 'mocha';
+import { startServer } from '../server';
 
 describe('Array', function () {
+  before('starting server', async function () {
+    await startServer();
+  });
   describe('#indexOf()', function () {
     it('should return -1 when the value is not present', function () {
       console.log('Hello there! Testing...');
