8185adafed4b6e421d7458da3b75b11a0a00f0b4
davytautas <daniel.martins@taqtile.com.br>
2022-09-12 20:59:09 -0300

    Started test environment setup and added env variables

--- src/data-source.ts
+++ src/data-source.ts
@@ -1,14 +1,17 @@
 import 'reflect-metadata';
 import { DataSource } from 'typeorm';
 import { User } from './entity/User';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
 
 export const AppDataSource = new DataSource({
   type: 'postgres',
   host: 'localhost',
-  port: 5432,
-  username: 'db-admin',
-  password: 'taq1234',
-  database: 'local-data',
+  port: Number(process.env.DB_PORT),
+  username: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
   synchronize: true,
   logging: false,
   entities: [User],
