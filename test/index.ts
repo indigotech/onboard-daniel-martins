e3d97609a5e5c62806d10867761ad8f75d903d1e
davytautas <daniel.martins@taqtile.com.br>
2022-09-13 16:06:50 -0300

    Separated individual tests

--- test/index.ts
+++ test/index.ts
@@ -14,10 +14,6 @@ import { validTest, shortPW, letterPW, numberPW, repeatEmail } from './createUse
 
 export const endpoint = 'http://localhost:3000/';
 
-/* 
-
-*/
-
 describe('Array', function () {
   before('starting server', async function () {
     dotenv.config({ path: './test.env' });
@@ -28,22 +24,28 @@ describe('Array', function () {
     await startDB();
   });
 
+  afterEach('clears database', async function () {
+    await clearDB();
+    await AppDataSource.destroy();
+  });
+
   it('should get user data back from the server', async function () {
     await validTest(); // Checks server response for a typical valid user input
   });
 
-  it('should refuse invalid passwords', async function () {
+  it('should refuse short passwords', async function () {
     await shortPW(); // Checks server response for password shorter than 6 characters
+  });
+
+  it('should refuse passwords without numbers', async function () {
     await letterPW(); // Checks server response for letter-only password
+  });
+
+  it('should refuse passwords without letters', async function () {
     await numberPW(); // Checks server response for nunmber-only password
   });
 
   it('should refuse emails already in database', async function () {
     await repeatEmail();
-  });
-
-  afterEach('clears database', async function () {
-    await clearDB();
-    await AppDataSource.destroy();
   });
 });
